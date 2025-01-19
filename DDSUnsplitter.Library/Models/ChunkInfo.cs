dfafa50009275f0ddf193f3464c65a65837156f4
Geoff Gerber <ggerber@gmail.com>
2025-01-19 11:36:28 -0500

    Add models

---
+++ DDSUnsplitter.Library/Models/ChunkInfo.cs
@@ -0,0 +1,10 @@
+﻿namespace DDSUnsplitter.Library.Models;
+
+public sealed class ChunkInfo
+{
+    public string FileName { get; set; }
+    public uint MipLevel { get; set; }
+    public uint OffsetInFile { get; set; }
+    public uint SizeInFile { get; set; }
+    public uint SideDelta { get; set; }
+}
